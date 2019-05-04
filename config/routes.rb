a48317de03e2945ca86d28aa13b2a6af4439a38b
ryo <0823.tr@gmail.com>
2019-05-04 21:41:49 +0900

    add reviews model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/new'
   devise_for :users
   root 'pages#index'
   get  'pages/show'
+  resources :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_095803) do
+ActiveRecord::Schema.define(version: 2019_05_04_123456) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "review"
+    t.string "image"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
