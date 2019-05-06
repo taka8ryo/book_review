29d9b4b2b53f5721f54c6e6aba1bd7ffd4c36310
ryo <0823.tr@gmail.com>
2019-05-06 20:52:37 +0900

    edit layout

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :show, :destroy]
+  before_action :authenticate_user!, only: [:show, :index, :ceate, :destroy]
   def index
   end
 
