44efb233538ea5c06d9209755d3cabe86d428050
ryo <0823.tr@gmail.com>
2019-05-06 12:34:57 +0900

    add all reviews to top-page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @reviews = Review.all
   end
 
   def show
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! only: [:new, :show, :destroy]
   def index
   end
 
